555600b5b1d6cf94238b1e9c691e66fe69c1bed5
John Ma <jcema@ust.hk>
2017-01-02 23:39:56 +0800

    Changed README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CraigParse
 Parses data from Craigslist and exports as CSV.
+
+Start with `node index.js`
--- index.js
+++ index.js
@@ -9,23 +9,37 @@ var
     category : 'apa',
     minAsk : '1000'
   };
- 
+
+var result = [];
+
 client
   .search(options, 'contact -realtor -agent -management -apartment -duplex -regency -www -pioneer -realty')
   .then((listings) => {
     // filtered listings (by price) 
     listings.forEach((listing) => {
-    	console.log(listing);
+    	// console.log(listing);
 
-    	// Get the detals
-    	// client.details(listing)
-    	// .then((details) => {
-    	// 	console.log(details);
-    	// })
-  	})
+    	// Get the details
+    	client.details(listing)
+    	.then((details) => {
+		    details.forEach((item) => {
+				result.push({
+					pid: details.pid,
+					replyUrl: details.replyUrl,
+					title: details.title,
+					url: details.url,
+					postedAt: details.postedAt,
+					map: details.mapUrl
+				});
+		    });
+    	})
+  	});
+
+  	console.log(result);
+
 
   	// export listings as CSV
-  	var csv_data = baby.unparse(listings);
+  	var csv_data = baby.unparse(result);
   	fs.writeFile('result.csv', csv_data, function (err) {
 	  if (err) return console.log(err);
 	  console.log('exported data to result.csv');
